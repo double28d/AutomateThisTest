4017fb99eadff8cb9f78b208164a3bb2f846847c
double28d <daniel.danilov28@gmail.com>
2024-05-03 19:47:22 +0300

    am creat browserFactory

--- src/test/java/SharedData/SharedData.java
+++ src/test/java/SharedData/SharedData.java
@@ -1,5 +1,6 @@
 package SharedData;
 
+import SharedData.browser.BrowserFactory;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
@@ -13,14 +14,7 @@ public class SharedData {
 
     @BeforeMethod   // => pre-condition
     public void prepareDriver(){
-        //Deschidem un browser de Chrome
-        webDriver = new ChromeDriver();
-        //accesam un URL
-        webDriver.get("https://demoqa.com/");
-        //maximizarea ferestrei
-        webDriver.manage().window().maximize();
-        //wait implicit - se pune in mod nromal la inceput de cod asa cum este pus !aici!
-        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        webDriver = new BrowserFactory().getBrowserInstance();
     }
 
     @AfterMethod  // => post-condition
---
+++ src/test/java/SharedData/browser/ChromeService.java
@@ -0,0 +1,46 @@
+package SharedData.browser;
+
+import PropertyUtility.PropertyUtility;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+import java.util.Map;
+
+public class ChromeService implements BrowserService {
+
+    private WebDriver webDriver;
+
+    @Override
+    public void openBrowser(Map<String, String> testData ) {
+        //aceasta metoda are ca scop sa deschida un Chrome cu configurarile noastre
+
+        ChromeOptions options = (ChromeOptions) getBrowserOptions(testData);
+        webDriver = new ChromeDriver(options);
+        webDriver.get(testData.get("url"));
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+    }
+
+    @Override
+    public Object getBrowserOptions(Map<String, String> testData) {
+        //configuram optiunile pentru browser-ul nostru
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments(testData.get("gpu"));
+        chromeOptions.addArguments(testData.get("infobars"));
+        chromeOptions.addArguments(testData.get("extensions"));
+        chromeOptions.addArguments(testData.get("sandbox"));
+        if (!testData.get("headless").isEmpty()){
+            chromeOptions.addArguments(testData.get("headless"));
+        }
+
+
+        return chromeOptions;
+    }
+
+    public WebDriver getWebDriver() {
+        return webDriver;
+    }
+}

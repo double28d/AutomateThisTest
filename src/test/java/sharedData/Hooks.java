80504cb11d5978071657b83e6622b9ffc1042d91
double28d <daniel.danilov28@gmail.com>
2024-05-07 20:11:57 +0300

    diverse modificari2

--- src/test/java/sharedData/Hooks.java
+++ src/test/java/sharedData/Hooks.java
@@ -3,6 +3,7 @@ package sharedData;
 import loggerUtillity.LoggerUtillity;
 import net.bytebuddy.implementation.bind.annotation.DefaultMethod;
 import org.openqa.selenium.bidi.log.Log;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -20,9 +21,14 @@ public class Hooks extends SharedData {
     }
 
     @AfterMethod
-    public void clearEnvironment(){
+    public void clearEnvironment(ITestResult result){
+        if (!result.isSuccess()){
+            LoggerUtillity.errorTest(result.getThrowable().getMessage());
+        }
         clearDriver();
         LoggerUtillity.infoTest("The driver is closed with SUCCESS");
         LoggerUtillity.finishTestCase(testName);
     }
+
+
 }
